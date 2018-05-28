a61b08079ff922caa486e901d0f65d763c905296
sooryagangarajk <sooryagangaraj@gmail.com>
2018-05-28 17:22:12 +0530

    Initial Commit

---
+++ 3DObjectMaker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3DObjectMaker
+//
+//  Created by soorya gangaraj k on 28/05/18.
+//  Copyright © 2018 SG_Creations. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
